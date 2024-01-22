7f11ad533bbcd8f44150b225d5ed8fe6a53d329b
naive <33138729+xinkeji@users.noreply.github.com>
2024-01-23 00:24:59 +0800

    更新 elog.config.yuque-pwd.js

--- elog/elog.config.yuque-pwd.js
+++ elog/elog.config.yuque-pwd.js
@@ -25,7 +25,7 @@ module.exports = {
   platform: 'github',
   github: {
     user: process.env.LGH_USER,
-    token: process.env.GITHUB_TOKEN,
+    token: process.env.LGH_TOKEN,
     repo: process.env.LGH_REPO,
     prefixKey: '',
     host: 'cdn.jsdelivr.net'
