51da449005ba66ed8c6b37b3958fb42dd90d7147
naive <33138729+xinkeji@users.noreply.github.com>
2024-02-03 01:13:05 +0800

    更新 elog.config.yuque-pwd.js

--- elog/elog.config.yuque-pwd.js
+++ elog/elog.config.yuque-pwd.js
@@ -25,7 +25,7 @@ module.exports = {
     platform: 'local',
     local: {
       outputDir: './source/images/yuque',
-      prefixKey: '/images/yuque'
+      prefixKey: '/images/yuque',
       host: 'cdn.jsdelivr.net/gh/xinkeji/xinkeji.github.io'
     }
   },
